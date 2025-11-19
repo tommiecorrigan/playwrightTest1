056f612d650d72aa9c6680f8628557e0f81c36e0
tommiecorrigan <tommie-corrigan@hotmail.com>
2025-11-19 14:10:35 -0800

    update viewport size

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
-    viewport: null,
+    viewport: { width: 1920, height: 1080 },
     launchOptions: {
       args: ['--start-maximized'],
       },
